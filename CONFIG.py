5d4ae4ba6b73c8add6b58047a7c05d03269292bd
ForensicatorEC <86324816+ForensicatorEC@users.noreply.github.com>
2021-06-30 12:33:02 -0400

    Create CONFIG.py

---
+++ CONFIG.py
@@ -0,0 +1,9 @@
+DESTINATION_DIR="FYTDL ACQUIRED VIDEOS"
+CSV_FILE="RESULTS.csv"
+DEBUG_MODE = True
+LOG_FILE = "FYTDL_log.log"
+LOG_FORMAT = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+
+YDL_VIDEO_LIST_FILE="VIDEO_LIST_FILE.txt"
+YDL_OUTPUT_PARAM="./FYTDL ACQUIRED VIDEOS/%(title)s-%(id)s.%(ext)s"
+YDL_FORMAT_PARAM="best"
